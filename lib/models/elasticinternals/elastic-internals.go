9ad430d995b20d73e39b90fd47a6934da182f483
Parallels <parallels@boxuser@ubuntu.local>
2020-12-02 18:28:52 -0700

    updated GetTaskIdResponse to enclude response object returned when a task is complete; failure object is []string which is probably wrong

--- lib/models/elasticinternals/elastic-internals.go
+++ lib/models/elasticinternals/elastic-internals.go
@@ -27,7 +27,28 @@ type CatIndex struct {
 type GetTaskIdResponse struct {
 	Completed bool       `json:"completed"`
 	Task      TaskObject `json:"task"`
+	// when a task is completed, it will have response objet with same/smilar meta as task
+	Response ResponseObject `json:"response"`
 }
+type ResponseObject struct {
+	Took                 int64         `json:"took"`
+	TimedOut             int64         `json:"timed_out"`
+	Total                int64         `json:"total"`
+	Updated              int64         `json:"updated"`
+	Created              int64         `json:"created"`
+	Deleted              int64         `json:"deleted"`
+	Batches              int64         `json:"batches"`
+	VersionConflicts     int64         `json:"version_conflicts"`
+	Noops                int64         `json:"nops"`
+	Retries              RetriesObject `json:"retries"`
+	Throttled            string        `json:"throttled"`
+	ThrottledMillis      int64         `json:"throttled_millis"`
+	RequestsPerSecond    float64       `json:"requests_per_second"`
+	ThrottledUntil       string        `json:"thorttled_until"`
+	ThrottledUntilMillis int64         `json:"throttled_until_milis"`
+	Failures             []string      `json:"failures"`
+}
+
 type TaskObject struct {
 	Node             string `json:"node"`
 	Id               int64  `json:"id"`
